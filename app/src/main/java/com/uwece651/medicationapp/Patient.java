eb1dbbefb7d0963dbd1797f2e0e53c9d9b52c702
JaredSoftEng <55007648+JaredSoftEng@users.noreply.github.com>
2020-01-28 22:18:46 -0500

    Sample Class

--- app/src/main/java/com/uwece651/medicationapp/Patient.java
+++ app/src/main/java/com/uwece651/medicationapp/Patient.java
@@ -1,4 +1,20 @@
 package com.uwece651.medicationapp;
 
 public class Patient {
+    // Each user is identified by a Used ID, this is also linked to their Google Sign-in so that we can automatically determine
+    // which user dataset the currently logged in user is associated with.
+
+    // Data Required:
+    // 1 UserID
+    // 1 Associated_Prescriptions (Array of Prescription IDs)
+    // 1 Last_Visit
+    // 1 Visit_History_Ids (array of numeric IDs)
+    // 1 Assigned_Doctor
+
+    // Public Class Visit_Info {
+    //   1 Visit_ID
+    //   1 Visit_DateTime
+    //   1 Visit_Duration
+    //   1
+    // }
 }
