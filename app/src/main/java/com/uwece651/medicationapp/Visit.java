2d7e2c07b0bb95e4e7e865849d20717b6b69614e
Hammad Rehman <harehman@uwaterloo.ca>
2020-01-31 23:10:47 -0500

    finished (I think) setting up all people type classes

--- app/src/main/java/com/uwece651/medicationapp/Visit.java
+++ app/src/main/java/com/uwece651/medicationapp/Visit.java
@@ -1,4 +1,23 @@
 package com.uwece651.medicationapp;
 
+import java.time.LocalDate;
+
 public class Visit {
+
+    private String visitID;
+    private LocalDate visitDate;
+    private int visitDuration; //duration in minutes
+
+    public Visit (String visitID, LocalDate visitDate, int visitDuration) {
+        this.visitID = visitID;
+        this.visitDate = visitDate;
+        this.visitDuration = visitDuration;
+    }
+
+    public String getVisitID () { return this.visitID;}
+
+    public LocalDate getVisitDate() { return this.visitDate;}
+
+    public int getVisitDuration () { return this.visitDuration;}
+
 }
