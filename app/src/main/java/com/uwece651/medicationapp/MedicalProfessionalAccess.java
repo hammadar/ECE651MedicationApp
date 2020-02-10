9246c999016c8d33e94c095fff5d31c08baedcec
Trishal <tsudula@uwaterloo.ca>
2020-02-09 22:24:02 -0500

    Medication data fields are now generated dynamically

--- app/src/main/java/com/uwece651/medicationapp/MedicalProfessionalAccess.java
+++ app/src/main/java/com/uwece651/medicationapp/MedicalProfessionalAccess.java
@@ -18,50 +18,35 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MedicalProfessionalAccess extends AppCompatActivity {
     private Button retrievePatientInfoButton;
     private Button addNewMedicationButton;
     private Button savePatientDataButton;
-    private EditText patientId, medicationName, timeBetweenIntake;
-    private Spinner timeBetweenIntakeUnits,dailyFrequency;
-    private CheckBox mondayCheckbox,tuesdayCheckbox,wednesdayCheckbox,thursdayCheckbox,fridayCheckbox,saturdayCheckbox,sundayCheckbox;
+    private EditText patientId;
 
-    private TextView medicationNameLabelObj, weeklyFrequencyLabelObj,dailyFrequencyLabelObj,timeBetweenIntakeLabelObj;
-    private LinearLayout checkboxLayout;
+    private int numberOfMedications=0;
 
-    private int numberOfMedications=1;
+    List<EditText> medicationNameEditTextList = new ArrayList<EditText>();
+    List<CheckBox> dayCheckboxList = new ArrayList<CheckBox>();
+    List<Spinner> timesPerDaySpinnerList = new ArrayList<Spinner>();
+    List<EditText> timeBetweenIntakeEditTextList = new ArrayList<EditText>();
+    List<Spinner> intakeUnitsSpinnerList = new ArrayList<Spinner>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_professional_access);
 
-        medicationNameLabelObj=findViewById(R.id.medicationNameLabel);
-        weeklyFrequencyLabelObj=findViewById(R.id.weeklyFrequencyLabel);
-        dailyFrequencyLabelObj=findViewById(R.id.dailyFrequencyLabel);
-        timeBetweenIntakeLabelObj=findViewById(R.id.dailyFrequencyLabel);
-
         retrievePatientInfoButton= findViewById(R.id.retrievePatientInfo);
         addNewMedicationButton= findViewById(R.id.addNewMedication);
         savePatientDataButton= findViewById(R.id.savePatientData);
 
         patientId=findViewById(R.id.patientId);
-        medicationName=findViewById(R.id.medicationNameTextBox);
-        timeBetweenIntake=findViewById(R.id.timeBetweenIntakeTextBox);
-
-        timeBetweenIntakeUnits=findViewById(R.id.timeBetweenIntakeUnitsDropBox);
-        dailyFrequency=findViewById(R.id.dailyFrequencyDropBox);
-
-        checkboxLayout=findViewById(R.id.checkBoxLayout);
-        mondayCheckbox= findViewById(R.id.mondayCheckBox);
-        tuesdayCheckbox= findViewById(R.id.tuesdayCheckBox);
-        wednesdayCheckbox= findViewById(R.id.wednesdayCheckBox);
-        thursdayCheckbox= findViewById(R.id.thursdayCheckBox);
-        fridayCheckbox= findViewById(R.id.fridayCheckBox);
-        saturdayCheckbox= findViewById(R.id.saturdayCheckBox);
-        sundayCheckbox= findViewById(R.id.sundayCheckBox);
-
-
 
         retrievePatientInfoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,8 +83,7 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
     public void addNewMedication(View view){
         numberOfMedications++;
 
-
-        TableLayout tl = findViewById(R.id.patientDataTableLayout);
+        TableLayout tl = findViewById(R.id.medicationDataTableLayout);
 
         //Medication Name
         TableRow tr = new TableRow(this);
@@ -107,62 +91,62 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
         LinearLayout ll= new LinearLayout(this);
         TextView tv = new TextView(this);
         tv.setText("Medication "+numberOfMedications+ " Name:");
-        tv.setHeight(medicationNameLabelObj.getHeight());
-        tv.setWidth(medicationNameLabelObj.getWidth());
 
         EditText et= new EditText(this);
         et.setWidth(500);
+        medicationNameEditTextList.add(et);
 
         //Weekly Frequency
         TableRow tr1 = new TableRow(this);
         LinearLayout ll1= new LinearLayout(this);
 
         ll1.setOrientation(LinearLayout.HORIZONTAL);
-//        ll1.setMinimumHeight(checkboxLayout.getHeight());
-//        ll1.setMinimumWidth(checkboxLayout.getWidth());
-        TextView tv1 = new TextView(this);
-        tv1.setText("Weekly Frequency:");
-//        tv.setHeight(weeklyFrequencyLabelObj.getHeight());
-//        tv.setWidth(weeklyFrequencyLabelObj.getWidth());
 
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT);
-//        params.leftMargin = 10;
+        TextView tv1 = new TextView(this);
+        tv1.setText("Days:");
 
 
         CheckBox cb = new CheckBox(this);
         cb.setText("S");
         cb.setGravity(Gravity.CENTER);
-        cb.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb);
+//        cb.setBackgroundResource(R.drawable.checkbox_weekday);
 
         CheckBox cb1 = new CheckBox(this);
         cb1.setText("M");
         cb1.setGravity(Gravity.CENTER);
-        cb1.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb1);
+//        cb1.setButtonDrawable(R.drawable.checkbox_weekday);
 
         CheckBox cb2 = new CheckBox(this);
         cb2.setText("T");
         cb2.setGravity(Gravity.CENTER);
-        cb2.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb2);
+//        cb2.setButtonDrawable(R.drawable.checkbox_weekday);
 
         CheckBox cb3 = new CheckBox(this);
         cb3.setText("W");
         cb3.setGravity(Gravity.CENTER);
-        cb3.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb3);
+//        cb3.setButtonDrawable(R.drawable.checkbox_weekday);
 
         CheckBox cb4 = new CheckBox(this);
         cb4.setText("T");
         cb4.setGravity(Gravity.CENTER);
-        cb4.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb4);
+//        cb4.setButtonDrawable(R.drawable.checkbox_weekday);
 
         CheckBox cb5 = new CheckBox(this);
         cb5.setText("F");
         cb5.setGravity(Gravity.CENTER);
-        cb5.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb5);
+//        cb5.setButtonDrawable(R.drawable.checkbox_weekday);
 
         CheckBox cb6 = new CheckBox(this);
         cb6.setText("S");
         cb6.setGravity(Gravity.CENTER);
-        cb6.setButtonDrawable(sundayCheckbox.getBackground());
+        dayCheckboxList.add(cb6);
+//        cb6.setButtonDrawable(R.drawable.checkbox_weekday);
 
         //Daily Frequency
         TableRow tr2 = new TableRow(this);
@@ -170,16 +154,13 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
 
         TextView tv2 = new TextView(this);
         tv2.setText("Times per day:");
-//        tv2.setHeight(dailyFrequencyLabelObj.getHeight());
-//        tv2.setWidth(dailyFrequencyLabelObj.getWidth());
 
         Spinner dailyFrequencySpinner = new Spinner(this);
-//        dailyFrequencySpinner.setMinimumHeight(dailyFrequency.getHeight());
-//        dailyFrequencySpinner.setMinimumWidth(dailyFrequency.getWidth());
         final String[] dailyFrequencyArray = getResources().getStringArray(R.array.medication_daily_frequency);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, dailyFrequencyArray);
         dailyFrequencySpinner.setAdapter(arrayAdapter);
+        timesPerDaySpinnerList.add(dailyFrequencySpinner);
 
         //Time between intake
         TableRow tr3 = new TableRow(this);
@@ -187,26 +168,17 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
 
         TextView tv3 = new TextView(this);
         tv3.setText("Times between intake:");
-//        tv3.setHeight(timeBetweenIntakeLabelObj.getHeight());
-//        tv3.setWidth(timeBetweenIntakeLabelObj.getWidth());
 
         EditText et1= new EditText(this);
         et1.setWidth(150);
-//        et1.setHeight(timeBetweenIntake.getHeight());
+        timeBetweenIntakeEditTextList.add(et1);
 
         Spinner intakeTimeUnitsSpinner = new Spinner(this);
-//        intakeTimeUnitsSpinner.setMinimumHeight(timeBetweenIntakeUnits.getHeight());
-//        intakeTimeUnitsSpinner.setMinimumWidth(timeBetweenIntakeUnits.getWidth());
         final String[] intakeTimeUnitsArray = getResources().getStringArray(R.array.time_units);
 
         ArrayAdapter intakeUnitsArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, intakeTimeUnitsArray);
         intakeTimeUnitsSpinner.setAdapter(intakeUnitsArrayAdapter);
-
-
-
-
-
-
+        intakeUnitsSpinnerList.add(intakeTimeUnitsSpinner);
 
         //Medication Name
         ll.addView(tv);
@@ -240,13 +212,6 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
         tr3.addView(ll3);
         tl.addView(tr3);
 
-
-
-
-
-
-
-
         //TODO: 27/01/20 Map medicationId with label maybe so we can keep track of medications
 
 
@@ -254,47 +219,69 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
     }
 
     public void savePatientData(){
+
         String patient_Id=patientId.getText().toString();
-        String medication_Name=medicationName.getText().toString();
-        Boolean isMondayChecked=mondayCheckbox.isChecked();
-        Boolean isTuesdayChecked=tuesdayCheckbox.isChecked();
-        Boolean isWednesdayChecked=wednesdayCheckbox.isChecked();
-        Boolean isThursdayChecked=thursdayCheckbox.isChecked();
-        Boolean isFridayChecked=fridayCheckbox.isChecked();
-        Boolean isSaturdayChecked=saturdayCheckbox.isChecked();
-        Boolean isSundayChecked=sundayCheckbox.isChecked();
-        String dailyFrequencyValue=dailyFrequency.getSelectedItem().toString();
-        String timeBetweenIntakeValue=timeBetweenIntake.getText().toString();
-        String timeBetweenIntakeUnitsValue=timeBetweenIntakeUnits.getSelectedItem().toString();
+        String medication_Name;
+        Boolean isMondayChecked;
+        Boolean isTuesdayChecked;
+        Boolean isWednesdayChecked;
+        Boolean isThursdayChecked;
+        Boolean isFridayChecked;
+        Boolean isSaturdayChecked;
+        Boolean isSundayChecked;
+        String dailyFrequencyValue;
+        String timeBetweenIntakeValue;
+        String timeBetweenIntakeUnitsValue;
 
-        PrescriptionData data= new PrescriptionData
-                (
-                        patient_Id//,
-                        /*medication_Name,
-                        isMondayChecked,
-                        isTuesdayChecked,
-                        isWednesdayChecked,
-                        isThursdayChecked,
-                        isFridayChecked,
-                        isSaturdayChecked,
-                        isSundayChecked,
-                        dailyFrequencyValue,
-                        timeBetweenIntakeValue,
-                        timeBetweenIntakeUnitsValue*/
-                );
+        for (int i = 0; i < medicationNameEditTextList.size(); i++) {
+            medication_Name=medicationNameEditTextList.get(i).getText().toString();
 
-        Log.d("MedicalProfAccess","patient_Id = " + patient_Id);
-        Log.d("MedicalProfAccess","medication_Name = " + medication_Name);
-        Log.d("MedicalProfAccess","isMondayChecked = " + isMondayChecked);
-        Log.d("MedicalProfAccess","isTuesdayChecked = " + isTuesdayChecked);
-        Log.d("MedicalProfAccess","isWednesdayChecked = " + isWednesdayChecked);
-        Log.d("MedicalProfAccess","isThursdayChecked = " + isThursdayChecked);
-        Log.d("MedicalProfAccess","isFridayChecked = " + isFridayChecked);
-        Log.d("MedicalProfAccess","isSaturdayChecked = " + isSaturdayChecked);
-        Log.d("MedicalProfAccess","isSundayChecked = " + isSundayChecked);
-        Log.d("MedicalProfAccess","dailyFrequencyValue = " + dailyFrequencyValue);
-        Log.d("MedicalProfAccess","timeBetweenIntakeValue = " + timeBetweenIntakeValue);
-        Log.d("MedicalProfAccess","timeBetweenIntakeUnitsValue = " + timeBetweenIntakeUnitsValue);
+            isSundayChecked=dayCheckboxList.get(i*7).isChecked();
+            isMondayChecked=dayCheckboxList.get(i*7 +1).isChecked();
+            isTuesdayChecked=dayCheckboxList.get(i*7 + 2).isChecked();
+            isWednesdayChecked=dayCheckboxList.get(i*7 + 3).isChecked();
+            isThursdayChecked=dayCheckboxList.get(i*7 + 4).isChecked();
+            isFridayChecked=dayCheckboxList.get(i*7 + 5).isChecked();
+            isSaturdayChecked=dayCheckboxList.get(i*7 + 6).isChecked();
+
+            dailyFrequencyValue=timesPerDaySpinnerList.get(i).getSelectedItem().toString();
+            timeBetweenIntakeValue=timeBetweenIntakeEditTextList.get(i).getText().toString();
+            timeBetweenIntakeUnitsValue=intakeUnitsSpinnerList.get(i).getSelectedItem().toString();
+
+            Log.d("MedicalProfAccess","patient_Id = " + patient_Id);
+            Log.d("MedicalProfAccess","medication_Name = " + medication_Name);
+            Log.d("MedicalProfAccess","isMondayChecked = " + isMondayChecked);
+            Log.d("MedicalProfAccess","isTuesdayChecked = " + isTuesdayChecked);
+            Log.d("MedicalProfAccess","isWednesdayChecked = " + isWednesdayChecked);
+            Log.d("MedicalProfAccess","isThursdayChecked = " + isThursdayChecked);
+            Log.d("MedicalProfAccess","isFridayChecked = " + isFridayChecked);
+            Log.d("MedicalProfAccess","isSaturdayChecked = " + isSaturdayChecked);
+            Log.d("MedicalProfAccess","isSundayChecked = " + isSundayChecked);
+            Log.d("MedicalProfAccess","dailyFrequencyValue = " + dailyFrequencyValue);
+            Log.d("MedicalProfAccess","timeBetweenIntakeValue = " + timeBetweenIntakeValue);
+            Log.d("MedicalProfAccess","timeBetweenIntakeUnitsValue = " + timeBetweenIntakeUnitsValue);
+            Log.d("MedicalProfAccess","\n\n");
+
+        }
+
+
+//        PrescriptionData data= new PrescriptionData
+//                (
+//                       // patient_Id,
+//                        /*medication_Name,
+//                        isMondayChecked,
+//                        isTuesdayChecked,
+//                        isWednesdayChecked,
+//                        isThursdayChecked,
+//                        isFridayChecked,
+//                        isSaturdayChecked,
+//                        isSundayChecked,
+//                        dailyFrequencyValue,
+//                        timeBetweenIntakeValue,
+//                        timeBetweenIntakeUnitsValue*/
+//                );
+
+
 
     }
 }
