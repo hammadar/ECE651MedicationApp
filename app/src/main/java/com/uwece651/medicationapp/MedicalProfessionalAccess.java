0497b179c13bf9cf4badf7075a4674f60d2c61b1
Hammad Rehman <harehman@uwaterloo.ca>
2020-03-10 00:26:58 -0400

    fixed merging issues?

--- app/src/main/java/com/uwece651/medicationapp/MedicalProfessionalAccess.java
+++ app/src/main/java/com/uwece651/medicationapp/MedicalProfessionalAccess.java
@@ -1,6 +1,7 @@
 package com.uwece651.medicationapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -22,6 +23,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -820,5 +822,22 @@ public class MedicalProfessionalAccess extends AppCompatActivity {
         });
 
 
+    }
+
+    public void signOut() {
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Intent fireBaseUIIntent = new Intent(getBaseContext(), FirebaseUIActivity.class);
+                            startActivity(fireBaseUIIntent);
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+                            builder.setMessage("Sign out unsuccessful. Please try again.");
+                            builder.setTitle("Sign Out Failed");
+                        }
+                    }
+                });
     }
 }
