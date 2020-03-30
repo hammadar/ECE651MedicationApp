c981632785b37a7e74b3574a3f79a9534fe78d39
Hammad Rehman <harehman@uwaterloo.ca>
2020-03-30 17:34:01 -0400

    temp push

---
+++ app/src/test/java/com/uwece651/medicationapp/MedicalProfessionalAccessTest.java
@@ -0,0 +1,38 @@
+package com.uwece651.medicationapp;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import android.support.test.filters.MediumTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static org.junit.Assert.*;
+
+public class MedicalProfessionalAccessTest {
+
+    private MedicalProfessionalAccess medicalProfessionalAccess;
+
+    @Rule
+    public ActivityTestRule
+    @Before()
+    public void before() {
+        medicalProfessionalAccess = new MedicalProfessionalAccess();
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+
+
+}
