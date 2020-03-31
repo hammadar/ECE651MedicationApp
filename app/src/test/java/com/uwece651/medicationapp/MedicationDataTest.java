38a582845deff93d3d2fc279a01fedc1c24636bc
JaredSoftEng <55007648+JaredSoftEng@users.noreply.github.com>
2020-03-31 16:11:11 -0400

    change MedData

--- app/src/test/java/com/uwece651/medicationapp/MedicationDataTest.java
+++ app/src/test/java/com/uwece651/medicationapp/MedicationDataTest.java
@@ -6,45 +6,44 @@ import static org.junit.Assert.*;
 
 public class MedicationDataTest {
 
-    private MedicationData medicationData = new MedicationData("1234");
-
     @Test
     public void All_MedicationDataTest() {
-        this.medicationData.setBrandName("BrandName");
-        this.medicationData.setContraindications("Weirdness");
-        this.medicationData.setDrugInteractions("Another Drug");
-        this.medicationData.setGenericName("Meh");
-        this.medicationData.setInstructions("Eat once a day");
-        this.medicationData.setMissedDose("2");
-        this.medicationData.setNotes("Urgh");
-        this.medicationData.setOverdose("5");
-        this.medicationData.setPrecautions("??");
-        this.medicationData.setSideEffects("Death");
-        this.medicationData.setStorage("Cold");
-        this.medicationData.setUsedFor("Fun");
+        MedicationData medicationData = new MedicationData("1234");
+        medicationData.setBrandName("BrandName");
+        medicationData.setContraindications("Weirdness");
+        medicationData.setDrugInteractions("Another Drug");
+        medicationData.setGenericName("Meh");
+        medicationData.setInstructions("Eat once a day");
+        medicationData.setMissedDose("2");
+        medicationData.setNotes("Urgh");
+        medicationData.setOverdose("5");
+        medicationData.setPrecautions("??");
+        medicationData.setSideEffects("Death");
+        medicationData.setStorage("Cold");
+        medicationData.setUsedFor("Fun");
         //-------------------------------------
-        assertEquals("Testing brand name: ", "BrandName", this.medicationData.getBrandName());
+        assertEquals("Testing brand name: ", "BrandName", medicationData.getBrandName());
         //-------------------------------------
-        assertEquals("Testing contraindictions: ", "Weirdness", this.medicationData.getContraindications());
+        assertEquals("Testing contraindictions: ", "Weirdness", medicationData.getContraindications());
         //---------------------------------------
-        assertEquals("Testing drug interactions: ", "Another Drug", this.medicationData.getDrugInteractions());
+        assertEquals("Testing drug interactions: ", "Another Drug", medicationData.getDrugInteractions());
         //--------------------------------------
-        assertEquals("Testing generic name: ", "Meh", this.medicationData.getGenericName());
+        assertEquals("Testing generic name: ", "Meh", medicationData.getGenericName());
         //------------------------------------
-        assertEquals("Testing instructions: ", "Eat once a day", this.medicationData.getInstructions());
+        assertEquals("Testing instructions: ", "Eat once a day", medicationData.getInstructions());
         //-------------------------------------
-        assertEquals("Testing missed dose: ", "2", this.medicationData.getMissedDose());
+        assertEquals("Testing missed dose: ", "2", medicationData.getMissedDose());
         //----------------------------------------
-        assertEquals("Testing Notes: ", "Urgh", this.medicationData.getNotes());
+        assertEquals("Testing Notes: ", "Urgh", medicationData.getNotes());
         //------------------------------------
-        assertEquals("Testing Overdose: ", "5", this.medicationData.getOverdose());
+        assertEquals("Testing Overdose: ", "5", medicationData.getOverdose());
         //--------------------------------
-        assertEquals("Testing precautions: ", "??", this.medicationData.getPrecautions());
+        assertEquals("Testing precautions: ", "??", medicationData.getPrecautions());
         //-------------------------------------
-        assertEquals("Testing side effects: ", "Death", this.medicationData.getSideEffects());
+        assertEquals("Testing side effects: ", "Death", medicationData.getSideEffects());
         //-----------------------------------------
-        assertEquals("Testing storage: ", "Cold", this.medicationData.getStorage());
+        assertEquals("Testing storage: ", "Cold", medicationData.getStorage());
         //------------------------------------------
-        assertEquals("Testing used for: ", "Fun", this.medicationData.getUsedFor());
+        assertEquals("Testing used for: ", "Fun", medicationData.getUsedFor());
     }
 }
